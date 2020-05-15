3456cb78772081685863432e89ca5e8edef8c0b9
acarolren <anacarolinareisdesa@gmail.com>
2020-05-15 01:35:35 -0300

    correção para erro

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+/*Created for solve an issue caused by usage of Vue CLI and also the usage of a component that had template defined as a string.
+[Vue warn]: You are using the runtime-only build of Vue where the template compiler is not available.
+Either pre-compile the templates into render functions, or use the compiler-included build.*/
+
+module.exports = {
+  runtimeCompiler: true
+}
